c62e1d787c58583acc327bdc8c45bd5278c1344b
Laura Degeratu <loveable_koneko@yahoo.com>
2019-01-26 14:42:15 +0100

    add map, create 5 markers

--- src/App.js
+++ src/App.js
@@ -1,28 +1,88 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Map, GoogleApiWrapper, Marker, InfoWindow } from 'google-maps-react'
 
-class App extends Component {
+export class App extends Component {
+  state = {
+    showingInfoWindow: false, //Hides or the shows the infoWindow
+    activeMarker: {}, //Shows the active marker upon click
+    selectedPlace: {} //Shows the infoWindow to the selected place upon a marker
+  }
+
+
+  onMarkerClick = (props, marker, e) => this.setState({
+    showingInfoWindow: true,
+    activeMarker: marker,
+    selectedPlace: props
+  });
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        activeMarker: null,
+        showingInfoWindow: false
+      });
+    }
+  };
+
   render() {
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
+        <div className="Search">
+          <input type="text" name="name" />
+        </div>
+        <Map
+          google={this.props.google}
+          zoom={14}
+          initialCenter={{
+            lat: 44.4314472,
+            lng: 26.097429
+          }}
+        >
+  
+          <Marker
+            onClick={this.onMarkerClick}
+            name={'The National Museum of Romanian History'}
+            position={{ lat: 44.4314472, lng: 26.097429 }}
+          />
+          <Marker
+            onClick={this.onMarkerClick}
+            name={'The National Museum of Art of Romania'}
+            position={{ lat: 44.4393759, lng: 26.0957858 }}
+          />
+          <Marker
+            onClick={this.onMarkerClick}
+            name={'Palace of Parliament'}
+            position={{ lat: 44.4275154, lng: 26.0872672 }}
+          />
+          <Marker
+            onClick={this.onMarkerClick}
+            name={'AFI Cotroceni Shopping Mall'}
+            position={{ lat: 44.4306195, lng: 26.0521446 }}
+          />
+          <Marker
+            onClick={this.onMarkerClick}
+            name={'The Fire Tower'}
+            position={{ lat: 44.4402447, lng: 26.1206101 }}
+          />
+
+          <InfoWindow
+            marker={this.state.activeMarker}
+            visible={this.state.showingInfoWindow}
+            onClose={this.onClose}
           >
-            Learn React
-          </a>
-        </header>
+            <div>
+              <h3>{this.state.selectedPlace.name}</h3>
+            </div>
+          </InfoWindow>
+        </Map>
       </div>
     );
   }
 }
 
-export default App;
+export default GoogleApiWrapper({
+  apiKey: 'AIzaSyBhwsiciOILeNSWQUMYcl9BmI1g0tSy33E'
+})(App);
