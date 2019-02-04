df82472b24674f4f25e372491e2c0cb09ed2875a
Laura Degeratu <loveable_koneko@yahoo.com>
2019-02-04 23:41:34 +0100

    register service worker as described above

--- src/index.js
+++ src/index.js
@@ -10,16 +10,5 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
 
-if ('serviceWorker' in navigator) {
-  window.addEventListener('load', function () {
-    navigator.serviceWorker.register('serviceWorker.js').then(function (registration) {
-      // Registration was successful
-      console.log('service worker registered');
-    }, function (err) {
-      // registration failed
-      console.log('service worker failed: ', err);
-    });
-  });
-}
-
+serviceWorker.register()
 
